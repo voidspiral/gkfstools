5c23b4ad9858faadd7d9329f0c5d27b2349ff130
lmj <lmj@t.com>
2024-09-13 10:20:56 +0800

    Fix fwrite can not be intercepted, write error info in mount directory

--- internal/metadata/intercept_write.go
+++ internal/metadata/intercept_write.go
@@ -1,33 +1,39 @@
 package metadata
 
 /*
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include <stdio.h>
-#include <stdlib.h>
-int writeToFile(const char *filePath, const char *data, size_t dataSize, size_t offset) {
-    FILE *output = fopen(filePath, "w");
 
-    if (output == NULL) {
+int writeToFile(const char *filePath, const char *data, size_t dataSize, size_t offset) {
+    int fd = open(filePath, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1) {
         perror("open file error");
         return 1;
     }
-    fseek(output, offset, SEEK_SET);
-    fwrite(data, sizeof(char), dataSize, output);
-	fclose(output);
+
+    ssize_t bytesWritten = pwrite(fd, data, dataSize, offset);
+    if (bytesWritten == -1) {
+        perror("pwrite error");
+        close(fd);
+        return 1;
+    }
+    close(fd);
     return 0;
 }
 */
 import "C"
 import (
 	"fmt"
-	"unsafe"
 )
 
 func WriteMetaData(filePath string, data []byte, offset uint64) error {
 	cFilePath := C.CString(filePath)
-	defer C.free(unsafe.Pointer(cFilePath))
+	//defer C.free(unsafe.Pointer(cFilePath))
 	//log.Println("Original data:", string(data))
 	cData := C.CBytes(data)
-	defer C.free(cData)
+	//defer C.free(cData)
 	//cDataBytes := C.GoBytes(cData, C.int(len(data)))
 	//log.Println("Converted C data:", string(cDataBytes))
 	//log.Println("WriteMetaData :", len(data))
